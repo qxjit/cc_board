64cc5b4c122b4b03b2eb6b8becd9d2a97826fc61
David Vollbracht <Administrator@DV5103.local>
2009-01-16 11:52:49 -0500

    display list of builds from builds directory

--- cc_board.rb
+++ cc_board.rb
@@ -3,7 +3,26 @@ $LOAD_PATH.unshift File.dirname(__FILE__) + "/vendor/sinatra-0.3.3/lib"
 
 require 'rubygems'
 require 'sinatra'
+require 'rexml/document'
+
+configure :production, :development do
+  BUILD_DATA_DIR = File.dirname(__FILE__) + "/builds"
+end
+
+configure :test do
+  BUILD_DATA_DIR = File.dirname(__FILE__) + "/test/builds"
+end
 
 get "/" do
-  "Hello Sinatra"
+  @builds = []
+
+  Dir[BUILD_DATA_DIR + "/*"].each do |filename|
+    doc = REXML::Document.new(File.read(filename))
+    doc.root.each_element("//Project") do |build_element|
+      @builds << build_element.attributes["name"]
+    end
+  end
+
+  erb :index
 end
+
--- test/index_test.rb
+++ test/index_test.rb
@@ -1,17 +1,30 @@
-$LOAD_PATH.unshift File.dirname(__FILE__) + "/vendor/rack-0.4.0/lib"
-$LOAD_PATH.unshift File.dirname(__FILE__) + "/vendor/sinatra-0.3.3/lib"
-$LOAD_PATH.unshift File.dirname(__FILE__) + "/vendor/thoughtbot-shoulda-2.0.6/lib"
+$LOAD_PATH.unshift File.dirname(__FILE__) + "/../vendor/rack-0.4.0/lib"
+$LOAD_PATH.unshift File.dirname(__FILE__) + "/../vendor/sinatra-0.3.3/lib"
 
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/test/unit'
 require File.dirname(__FILE__) + "/../cc_board"
 
+# Views is set relative to the test file rather than the app file
+Sinatra.options.views = File.dirname(__FILE__) + "/../views"
+
+$LOAD_PATH.unshift File.dirname(__FILE__) + "/../vendor/thoughtbot-shoulda-2.0.6/lib"
 require 'shoulda'
 
 class IndexTest < Test::Unit::TestCase
-  should "say hello" do
-    get_it "/"
-    assert_equal "Hello Sinatra", @response.body
+  def self.should_show_build(build_name)
+    should "show show build: #{build_name}" do
+      assert_match build_name, @response.body
+    end
+  end
+
+  context "the index page" do
+    setup { get_it "/" }
+
+    should_show_build "server1 build1"
+    should_show_build "server1 build2"
+    should_show_build "server2 build1"
+    should_show_build "server2 build2"
   end
 end
