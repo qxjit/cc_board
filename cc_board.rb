7785a81ee7063fa3176130a95049023acd73084d
David Vollbracht <Administrator@DV5103.local>
2009-01-24 14:39:07 -0500

    add asset versioning to stylesheets and javascript

--- cc_board.rb
+++ cc_board.rb
@@ -6,6 +6,22 @@ require 'sinatra'
 require 'lib/configuration'
 require 'lib/build_list'
 
+ASSET_DIR = File.dirname(__FILE__) + "/public/"
+
+helpers do
+  def stylesheet(name)
+    "<link href='#{versioned_asset(name + ".css")}' rel='stylesheet'/>"
+  end
+
+  def javascript(name)
+    "<script type='text/javascript' src='#{versioned_asset(name + ".js")}'></script>"
+  end
+
+  def versioned_asset(filename)
+    "#{filename}?#{File.mtime(ASSET_DIR + filename).to_i}"
+  end
+end
+
 get "/" do
   @build_lists = Dir[Configuration.build_data_dir + "/*"].map do |filename|
     BuildList.new filename
--- test/index_test.rb
+++ test/index_test.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/test_helper'
+require 'hpricot'
 
 class IndexTest < Test::Unit::TestCase
   def self.should_show_build(build_name)
@@ -14,5 +15,23 @@ class IndexTest < Test::Unit::TestCase
     should_show_build "server1 build2"
     should_show_build "server2 build1"
     should_show_build "server2 build2"
+
+    should "have all stylesheet and javascript assets versioned with timestamps" do
+      doc = Hpricot.parse(@response.body)
+      doc.get_elements_by_tag_name("link").each do |link|
+        assert_asset_versioned(link["href"]) if link["rel"] =~ /stylesheet/i
+      end
+
+      doc.get_elements_by_tag_name("script").each do |script|
+        assert_asset_versioned(script["src"]) if script["src"]
+      end
+    end
+  end
+  
+  def assert_asset_versioned(asset)
+    filename, version = asset.split('?')
+    flunk "#{filename} was not versioned" unless version
+    assert_equal version.to_i, File.mtime(File.dirname(__FILE__) + "/../public/#{filename}").to_i,
+                 "#{filename} was not given correct version"
   end
 end
